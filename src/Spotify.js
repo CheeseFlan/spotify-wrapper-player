8f6ce16d5fa6971553c76d13626b06ab206b2f53
John Henrique <joaoh3326@gmail.com>
2017-09-01 15:06:22 -0300

    Add new components

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: '',
+  token: 'BQB7opZpwTHhX2N9u1p17Pc4dZJSmjemH0-8UEMh5gClf7LVKt1LthYF35TXK-gsp7Jmj33RAwyxWrlpM3pG3A39HP6MK-KGSCVdcRyT5dPWUMy2Pls9OdfhqFxOdQ0PE7eoBNsJhA',
 });
 
 export default spotify;
